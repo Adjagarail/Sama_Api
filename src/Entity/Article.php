6d9a481c02ed1cceb07a68086dea875aaf62cc1f
Adjagarail <43035463+Adjagarail@users.noreply.github.com>
2019-01-08 23:52:08 +0000

    Creation entite

---
+++ src/Entity/Article.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre_Article;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Descriptif;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Article;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $Date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Categorie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Auteur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitreArticle(): ?string
+    {
+        return $this->Titre_Article;
+    }
+
+    public function setTitreArticle(string $Titre_Article): self
+    {
+        $this->Titre_Article = $Titre_Article;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(string $Image): self
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    public function getDescriptif(): ?string
+    {
+        return $this->Descriptif;
+    }
+
+    public function setDescriptif(string $Descriptif): self
+    {
+        $this->Descriptif = $Descriptif;
+
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->Article;
+    }
+
+    public function setArticle(string $Article): self
+    {
+        $this->Article = $Article;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->Categorie;
+    }
+
+    public function setCategorie(string $Categorie): self
+    {
+        $this->Categorie = $Categorie;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): self
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+}
