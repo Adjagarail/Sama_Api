4bacaebf70d118e856a6763caad51deaa50c40c6
Adjagarail <yayaadjagarailsadio@gmail.com>
2019-01-12 14:54:59 +0000

    Table Competence

---
+++ src/Entity/Competence.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ApiResource()
+ * @Vich\Uploadable
+ * @ORM\Entity(repositoryClass="App\Repository\CompetenceRepository")
+ */
+class Competence
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $Image;
+
+     /**
+     * @Vich\UploadableField(mapping="article", fileNameProperty="Image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+    
+     public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(File $Image = null)
+    {
+        $this->imageFile = $Image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+    }
+
+    public function setImage(string $Image): self
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+}
